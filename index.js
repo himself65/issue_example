1c5dbec20df5936b0b060fb6577d015e9db3aa17
himself65 <himself65@outlook.com>
2019-06-27 18:42:11 +0800

    success version

--- index.js
+++ index.js
@@ -1,15 +1,4 @@
-const assert = require('assert')
 const { KCP } = require('./build/Release/napi_test')
 
 const kcp = new KCP()
-try {
-  kcp.test()
-} catch (e) {
-  assert.ok(e instanceof Error)
-}
-
-try {
-  kcp.test('')
-} catch (e) {
-  assert.ok(e instanceof TypeError)
-}
+kcp.test()
